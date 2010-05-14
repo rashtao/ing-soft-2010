c09ac523a7bb0b74a62376588f26851303821089
rashtao <rashtao@e430718a-7904-8337-e0a1-c03b00660a90>
2010-05-14 09:23:56 +0000

    commit progetto clientsoap iniziale

---
+++ clientsoap/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: rashta
+ *
+ * Created on 14 maggio 2010, 11.23
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
