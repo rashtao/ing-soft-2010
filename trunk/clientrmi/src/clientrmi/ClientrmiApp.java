080a03d5377a331e92e6ba3ea7ba41ef16cac305
rashtao <rashtao@e430718a-7904-8337-e0a1-c03b00660a90>
2010-05-14 09:16:30 +0000

    commit progetto clientrmi iniziale

---
+++ trunk/clientrmi/src/clientrmi/ClientrmiApp.java
@@ -0,0 +1,44 @@
+/*
+ * ClientrmiApp.java
+ */
+
+package clientrmi;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class ClientrmiApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new ClientrmiView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of ClientrmiApp
+     */
+    public static ClientrmiApp getApplication() {
+        return Application.getInstance(ClientrmiApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(ClientrmiApp.class, args);
+    }
+}
