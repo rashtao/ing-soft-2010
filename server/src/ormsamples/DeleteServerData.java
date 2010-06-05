f7c6cd3fdc255229053a39bfdfe380b991aba90c
dmorelli86@gmail.com <dmorelli86@gmail.com@e430718a-7904-8337-e0a1-c03b00660a90>
2010-06-05 21:49:05 +0000

    update

--- server/src/ormsamples/DeleteServerData.java
+++ server/src/ormsamples/DeleteServerData.java
@@ -4,13 +4,30 @@
  */
 package ormsamples;
 
+import entity.EContatto;
+import entity.EContattoCriteria;
+import entity.ServerPersistentManager;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.CriteriaQuery;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.engine.TypedValue;
 import org.orm.*;
+import org.orm.criteria.StringExpression;
 public class DeleteServerData {
 	public void deleteTestData() throws PersistentException {
 		PersistentTransaction t = entity.ServerPersistentManager.instance().getSession().beginTransaction();
 		try {
-			entity.EContatto entityEContatto = entity.EContattoFactory.loadEContattoByQuery(null, null);
-			entityEContatto.delete();
+			EContattoCriteria contCrit=new EContattoCriteria(ServerPersistentManager.instance().getSession());
+                        StringExpression ert=new StringExpression("pippo", contCrit);
+                        contCrit.nome.like("pipp%");
+                        EContatto e[]=contCrit.listEContatto();
+                       // entity.EContatto entityEContatto = entity.EContattoFactory.loadEContattoByQuery(null, null);
+		//	entityEContatto.delete();
+                        for (int i=0;i<e.length; i++)
+                            e[i].delete();
+
 			entity.EGruppo entityEGruppo = entity.EGruppoFactory.loadEGruppoByQuery(null, null);
 			entityEGruppo.delete();
 			t.commit();
