698f25640d0a9cd8cdb6f7f2f102ac0c852c131b
dmorelli86@gmail.com <dmorelli86@gmail.com@e430718a-7904-8337-e0a1-c03b00660a90>
2010-06-06 16:21:08 +0000

    creazione metodi classi control

--- trunk/server/src/Control/CContatto.java
+++ trunk/server/src/Control/CContatto.java
@@ -2,6 +2,9 @@ package Control;
 
 import Entity.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.orm.PersistentException;
 
 public class CContatto {
 
@@ -28,40 +31,71 @@ public class CContatto {
 	 * @return 
 	 */
 	public synchronized boolean salvaContatto(EContatto contatto) {
-		throw new UnsupportedOperationException();
-	}
+            try {
+                return contatto.save();
+            } catch (PersistentException ex) {
+                Logger.getLogger(CContatto.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return false;
+        }
 
 	/**
 	 *
 	 * @param contatto
 	 */
-	public synchronized boolean eliminaContatto(EContatto contatto) {
-		throw new UnsupportedOperationException();
-	}
+	public synchronized boolean eliminaContatto(EContatto contatto) throws PersistentException {
+            return contatto.delete();
+        }
 
 	/**
 	 *
 	 * @param gruppo
 	 */
-	public Collection<EContatto> getByGroup(EGruppo gruppo) {
-		throw new UnsupportedOperationException();
-	}
+	public Collection<EContatto> getByGroup(EGruppo gruppo) throws PersistentException {
+            EContattoCriteria crit=new EContattoCriteria(ServerPersistentManager.instance().getSession());
+            if (!gruppo.getNome().equals(null))
+                crit.createEGruppoCriteria().nome.like("%"+gruppo.getNome()+"%");
+            if (!(new Integer(gruppo.getIdGruppo())==null))
+                crit.createEGruppoCriteria().idGruppo.eq(gruppo.getIdGruppo());
+            EContatto[] array=crit.listEContatto();
+            List<EContatto> list=new LinkedList<EContatto>();
+            for (int i=0; i<array.length; i++)
+                list.add(array[i]);
+            return list;
+        }
 
 	/**
 	 * 
 	 * @return 
 	 */
 	public EContatto nuovoContatto() {
-		throw new UnsupportedOperationException();
-	}
+            return new EContatto();
+        }
 
 	/**
 	 * 
 	 * @param likeContatto
 	 * @return 
 	 */
-	public Collection<EContatto> cercaContatto(EContatto likeContatto) {
-		throw new UnsupportedOperationException();
-	}
+	public Collection<EContatto> cercaContatto(EContatto likeContatto) throws PersistentException {
+            EContattoCriteria crit=new EContattoCriteria(ServerPersistentManager.instance().getSession());
+            if (!likeContatto.getNome().equals(null))
+                crit.nome.like("%"+likeContatto.getNome()+"%");
+            if (!likeContatto.getCognome().equals(null))
+                crit.cognome.like("%"+likeContatto.getCognome()+"%");
+            if (!likeContatto.getEmail().equals(null))
+                crit.email.like("%"+likeContatto.getEmail()+"%");
+            if (!likeContatto.getTelefono().equals(null))
+                crit.telefono.like("%"+likeContatto.getTelefono()+"%");
+            if (!likeContatto.geteGruppo().equals(null)){
+                crit.createEGruppoCriteria().idGruppo.eq(likeContatto.geteGruppo().getIdGruppo());
+                crit.createEGruppoCriteria().nome.eq(likeContatto.geteGruppo().getNome());
+            }
+            EContatto[] array=crit.listEContatto();
+            List<EContatto> list=new LinkedList<EContatto>();
+            for (int i=0; i<array.length; i++)
+                list.add(array[i]);
+            return list;
+        }
 
 }
--- trunk/server/src/Control/CGruppo.java
+++ trunk/server/src/Control/CGruppo.java
@@ -1,7 +1,9 @@
 package Control;
 
 import Entity.*;
+import Entity.EGruppoCriteria;
 import java.util.*;
+import org.orm.PersistentException;
 
 public class CGruppo {
 
@@ -28,33 +30,42 @@ public class CGruppo {
 	 * @param gruppo
 	 * @return 
 	 */
-	public synchronized boolean salvaGruppo(EGruppo gruppo) {
-		throw new UnsupportedOperationException();
-	}
+	public synchronized boolean salvaGruppo(EGruppo gruppo) throws PersistentException {
+            return gruppo.save();
+        }
 
 	/**
 	 * 
 	 * @param gruppo
 	 */
-	public synchronized boolean eliminaGruppo(EGruppo gruppo) {
-		throw new UnsupportedOperationException();
-	}
+	public synchronized boolean eliminaGruppo(EGruppo gruppo) throws PersistentException {
+            return gruppo.delete();
+        }
 
 	/**
 	 * 
 	 * @return 
 	 */
 	public EGruppo nuovoGruppo() {
-		throw new UnsupportedOperationException();
-	}
+            return new EGruppo();
+        }
 
 	/**
 	 * 
 	 * @param likeGruppo
 	 * @return 
 	 */
-	public Collection<EGruppo> cercaGruppo(EGruppo likeGruppo) {
-		throw new UnsupportedOperationException();
-	}
+	public Collection<EGruppo> cercaGruppo(EGruppo likeGruppo) throws PersistentException {
+            EGruppoCriteria crit=new EGruppoCriteria(ServerPersistentManager.instance().getSession());
+            if (!likeGruppo.getNome().equals(null))
+                crit.nome.like("%"+likeGruppo.getNome()+"%");
+            if (!(new Integer(likeGruppo.getIdGruppo())==null))
+                crit.idGruppo.eq(likeGruppo.getIdGruppo());
+            EGruppo[] array=crit.listEGruppo();
+            List<EGruppo> list=new LinkedList<EGruppo>();
+            for (int i=0; i<array.length; i++)
+                list.add(array[i]);
+            return list;
+        }
 
 }
